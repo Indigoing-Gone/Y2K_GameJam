a743058dcc6d35e081a01f433bed34ae9ec900a4
Indigoing_Gone <wynnmcwhirter@gmail.com>
2026-04-17 17:06:05 -0400

    [ADD] Shop and basic game loop

---
+++ Y2K_GameJam/Assets/Programming/Shop/ShopSlot.cs
@@ -0,0 +1,49 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    public Action<ClothingData> OnShopSlotSelected;
+
+    [Header("Shop Slot Info")]
+    [SerializeField] private ClothingData clothingData;
+    [SerializeField] private GameObject clothingImageObject;
+    private Image clothingImage;
+    private AspectRatioFitter clothingRatio;
+
+    void Awake()
+    {
+        clothingImage = clothingImageObject.GetComponent<Image>();
+        clothingRatio = clothingImageObject.GetComponent<AspectRatioFitter>();
+    }
+
+    public void UpdateData(ClothingData _data)
+    {
+        clothingData = _data;
+
+        if(_data == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        if(_data.Sprite == null)
+        {
+            clothingImage.sprite = null;
+            clothingRatio.aspectRatio = 1f;
+            return;
+        }
+        
+        clothingImage.sprite = _data.Sprite;
+        clothingRatio.aspectRatio = _data.Sprite.bounds.size.x / _data.Sprite.bounds.size.y;
+    }
+
+    public void SelectShopSlot()
+    {
+        if (clothingData == null) return;
+        OnShopSlotSelected?.Invoke(clothingData);
+        UpdateData(null);
+    }
+}
