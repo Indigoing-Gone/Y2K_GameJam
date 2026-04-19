01222a03d9550d83d4c31bd86153890adaea8bdc
GarrettJesWade <GarrettJesWade@gmail.com>
2026-04-19 15:28:12 -0400

    [ADD] Tutorial Panel

--- Y2K_GameJam/Assets/Programming/UI/Menu.cs
+++ Y2K_GameJam/Assets/Programming/UI/Menu.cs
@@ -3,9 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] GameObject tutorialPanel;
     [SerializeField] GameObject creditPanel;
     
     public void StartGame() => SceneManager.LoadScene("MainScene");
 
+    public void ToggleTutorial(bool _creditsStatus) => tutorialPanel.SetActive(_creditsStatus);
+
     public void ToggleCredits(bool _creditsStatus) => creditPanel.SetActive(_creditsStatus);
+
 }
